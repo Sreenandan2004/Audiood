5eded685fad0d9ae06bffc8c7e8dcf2b3410cc16
Sreenandan2004 <75662296+Sreenandan2004@users.noreply.github.com>
2026-02-13 23:31:47 +0530

    logic: audio models created

---
+++ lib/models/models.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VoiceNote {
+  final String title;
+  final String mood;
+  final String duration;
+
+  VoiceNote({required this.duration, required this.mood, required this.title});
+}
+
+class FriendProfile {
+  final String name;
+  final String imagePath;
+  final List<VoiceNote> voiceNotes;
+
+  FriendProfile({
+    required this.name,
+    required this.voiceNotes,
+    required this.imagePath,
+  });
+}
