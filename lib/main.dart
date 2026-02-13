a86601847df6bfbb661d47225cb52adf420b64e7
Sreenandan2004 <75662296+Sreenandan2004@users.noreply.github.com>
2026-02-13 23:39:18 +0530

    Assets added

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false);
+    return MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false);
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import '../models/models.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // Dummy data for now
+  final List<FriendProfile> profiles = [
+    FriendProfile(name: "ZONAL", imagePath: "assets/zonal.jpg", voiceNotes: []),
+    FriendProfile(name: "ABHAI", imagePath: "assets/abhai.jpg", voiceNotes: []),
+    // Add more...
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: profiles.length,
+        itemBuilder: (context, index) {
+          final profile = profiles[index];
+          return Stack(
+            children: [
+              // Layer 1: The Background Image
+              // TODO: How do we make this fill the screen?
+
+              // Layer 2: The Gradient/Overlay for readability
+
+              // Layer 3: The Content (Header + List)
+            ],
+          );
+        },
+      ),
+    );
   }
 }
